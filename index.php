b2ebd15e13bdbb8e1db031a9ee3a260d7ab03c79
Arseniy <fokinarseniy69@gmail.com>
2012-12-13 17:02:18 +0800

    Усложнил систему страниц, добавил поддержку стилей, дозаполнил форму

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8');
 	
-	if (isset($_GET['gen'])):
-	
+	if (isset($_GET['gen']))
+	{	
 		include 'forms/pack_gen_form.inc';
-	
-	elseif (isset($_GET['compare'])):
-
+	}
+	elseif (isset($_GET['compare']))
+	{
 		include 'forms/pack_compare_form.inc';
-	
-	else:
-?>
-	
-	Выберете тест:<br>
-	<p><a href="?gen" style="text-decoration: none"><input type="button" value="Simple Packet Generator" /></a></p>
-	<p><a href="?compare" style="text-decoration: none"><input type="button" value="Packets Compare" /></a></p>
-
-<?php
-	endif;	
+	}
+	elseif (isset($_GET['parse']))
+	{
+		include 'forms/pack_parse_form.inc';
+	}
+	else
+	{
+		include 'forms/main_menu.inc';
+	}
 ?>
